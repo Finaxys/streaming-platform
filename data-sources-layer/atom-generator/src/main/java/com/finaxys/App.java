685b3ef1c23129d4dcfc7c861627885f9878c032
adouang <alain.douangpraseuth@gmail.com>
2016-01-05 15:41:47 +0100

    Create project parent and add atom-generator module

---
+++ data-sources-layer/atom-generator/src/main/java/com/finaxys/App.java
@@ -0,0 +1,13 @@
+package com.finaxys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
