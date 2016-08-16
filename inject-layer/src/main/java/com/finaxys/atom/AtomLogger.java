fecaeea9cef82dd64e97c298a4db2763164287a6
raphaelhamonnais <raphael.hamonnais@gmail.com>
2016-08-16 17:38:25 +0200

    Add comments with logs semantics

--- inject-layer/src/main/java/com/finaxys/atom/AtomLogger.java
+++ inject-layer/src/main/java/com/finaxys/atom/AtomLogger.java
@@ -41,6 +41,7 @@ public class AtomLogger extends Logger {
 
     @Override
     public void agent(Agent a, Order o, PriceRecord pr) {
+        // Semantic of an Agent log : agent;name;cash;obName;nbInvest;lastFixedPrice
         super.agent(a, o, pr);
         long ts = tsb.incrementTimeStampByOrderMillis();
         for (AtomDataInjector injector : injectors) {
@@ -51,6 +52,7 @@ public class AtomLogger extends Logger {
 
     @Override
     public void exec(Order o) {
+        // Semantic of an Exec log : Exec;nameOfTheAgentThatSendTheOrder-OrderExtId
         super.exec(o);
         long ts = tsb.incrementTimeStampByOrderMillis();
         for (AtomDataInjector injector : injectors) {
@@ -60,6 +62,7 @@ public class AtomLogger extends Logger {
 
     @Override
     public void order(Order o) {
+        // Semantic of an Order log : order;obName;sender;extId;type;dir;price;quty;valid
         super.order(o);
         long ts = tsb.incrementTimeStampByOrderMillis();
         for (AtomDataInjector injector : injectors) {
@@ -69,6 +72,7 @@ public class AtomLogger extends Logger {
 
     @Override
     public void price(PriceRecord pr, long bestAskPrice, long bestBidPrice) {
+        // Semantic of a Price log : price;obName;price;executedQuty;dir;order1;order2;bestask;bestbid
         super.price(pr, bestAskPrice, bestBidPrice);
         long ts = tsb.incrementTimeStampByOrderMillis();
         for (AtomDataInjector injector : injectors) {
@@ -78,19 +82,8 @@ public class AtomLogger extends Logger {
 
 
     @Override
-    public void day(int nbDays, java.util.Collection<OrderBook> orderbooks) {
-        super.day(nbDays, orderbooks);
-
-        tsb.setCurrentDay(nbDays);
-        long ts = tsb.incrementTimeStampByOrderMillis();
-        for (AtomDataInjector injector : injectors) {
-            injector.sendDay(ts, nbDays, orderbooks);
-        }
-    }
-
-
-    @Override
     public void tick(Day day, java.util.Collection<OrderBook> orderbooks) {
+        // Semantic of a Tick log : tick;numTick;obName;bestask;bestbid;lastPrice
         super.tick(day, orderbooks);
         tsb.setCurrentTick(day.currentTick());
         tsb.setTimeStamp(tsb.computeTimestampForCurrentTick());
@@ -98,7 +91,17 @@ public class AtomLogger extends Logger {
         for (AtomDataInjector injector : injectors) {
             injector.sendTick(ts, day, orderbooks);
         }
+    }
 
+    @Override
+    public void day(int numOfDay, java.util.Collection<OrderBook> orderbooks) {
+        // Semantic of a Day log : day;NumDay;obName;FirstfixedPrice;LowestPrice;HighestPrice;lastFixedPrice;nbPricesFixed
+        super.day(numOfDay, orderbooks);
+        tsb.setCurrentDay(numOfDay);
+        long ts = tsb.incrementTimeStampByOrderMillis();
+        for (AtomDataInjector injector : injectors) {
+            injector.sendDay(ts, numOfDay, orderbooks);
+        }
     }
 
     public void close() throws Exception {
