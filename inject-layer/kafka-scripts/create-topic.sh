c573ba84cb886c9d6b724962f92f0fde43176afe
raphaelhamonnais <raphael.hamonnais@gmail.com>
2016-08-17 16:58:38 +0200

    Change Kafka topic name from "atomTopic" to "atom-raw-data"

--- inject-layer/kafka-scripts/create-topic.sh
+++ inject-layer/kafka-scripts/create-topic.sh
@@ -6,28 +6,28 @@ cd $KAFKA_HOME # or enter the direct path if you do not have an environment vari
 
 #Start Kafka
 # start zookeeper server
-./bin/zookeeper-server-start.sh ./config/zookeeper.properties &
+$KAFKA_HOME/bin/zookeeper-server-start.sh ./config/zookeeper.properties &
 
 # start broker
-./bin/kafka-server-start.sh ./config/server.properties &
+$KAFKA_HOME/bin/kafka-server-start.sh ./config/server.properties &
 
-# create topic named "atomTopic" (name is here as an example)
- ./bin/kafka-topics.sh --create --topic atomTopic --zookeeper localhost:2181 --partitions 1 --replication-factor 1
+# create topic named "atom-raw-data" (name is here as an example)
+$KAFKA_HOME/bin/kafka-topics.sh --create --topic atom-raw-data --zookeeper localhost:2181 --partitions 1 --replication-factor 1
 
 # consume from the topic using the console producer
-./bin/kafka-console-consumer.sh --topic atomTopic --zookeeper localhost:2181
+$KAFKA_HOME/bin/kafka-console-consumer.sh --topic atom-raw-data --zookeeper localhost:2181
 
 # produce something into the topic (write something and hit enter)
-./bin/kafka-console-producer.sh --topic atomTopic --broker-list localhost:9092
+$KAFKA_HOME/bin/kafka-console-producer.sh --topic atom-raw-data --broker-list localhost:9092
 
 
 
 # list topics
-./bin/kafka-topics.sh --list --zookeeper localhost:2181
+$KAFKA_HOME/bin/kafka-topics.sh --list --zookeeper localhost:2181
 
 # delete a topic
-./bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic your_topic_name
+$KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic your_topic_name
 
 # Stop Kafka instructions (in THIS order)
-./bin/kafka-server-stop.sh
-./bin/zookeeper-server-stop.sh
+$KAFKA_HOME/bin/kafka-server-stop.sh
+$KAFKA_HOME/bin/zookeeper-server-stop.sh
