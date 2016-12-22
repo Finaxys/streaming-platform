6e0f48b5102254fdcad0b5fb5286ea0dda6ee569
raphaelhamonnais <raphael.hamonnais@gmail.com>
2016-12-22 21:04:45 +0100

    Deletion of non necessary modules

--- persist-layer/hbase/create-table.hb
+++
@@ -1,1 +0,0 @@
-create 'trace', 'cf'
--- persist-layer/hbase/create-table.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export HBASE_PATH=/home/finaxys/hbase-1.1.2
-
-$HBASE_PATH/bin/hbase shell < ./delete-table.hb
-$HBASE_PATH/bin/hbase shell < ./create-table.hb
