f8615c1b97e04c3742cd53513c093fd171de2bdf
raphaelhamonnais <raphael.hamonnais@gmail.com>
2017-03-04 15:18:04 +0100

    Add AgentPosition jobs in the ProcessorFactory class

--- stream-layer/src/main/java/com/finaxys/flink/processor/ProcessorFactory.java
+++ stream-layer/src/main/java/com/finaxys/flink/processor/ProcessorFactory.java
@@ -1,5 +1,8 @@
 package com.finaxys.flink.processor;
 
+import com.finaxys.flink.processor.agentposition.AgentPositionEventTime;
+import com.finaxys.flink.processor.agentposition.AgentPositionProcessingTime;
+import com.finaxys.flink.processor.agentposition.AgentPositionReference;
 import com.finaxys.flink.processor.meanprice.PriceMeanEventTime;
 import com.finaxys.flink.processor.meanprice.PriceMeanProcessingTime;
 import com.finaxys.flink.processor.meanprice.PriceMeanReference;
@@ -14,21 +17,30 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
  */
 public class ProcessorFactory {
 
-    private static final String PRICE_MEAN_EVENT_TIME_PROCESSOR = "PriceMeanEventTime";
-    private static final String PRICE_MEAN_PROCESSING_TIME_PROCESSOR = "PriceMeanProcessingTime";
     private static final String PRICE_MEAN_REFERENCE = "PriceMeanReference";
+    private static final String PRICE_MEAN_EVENT_TIME = "PriceMeanEventTime";
+    private static final String PRICE_MEAN_PROCESSING_TIME = "PriceMeanProcessingTime";
+
+    private static final String AGENT_POSITION_REFERENCE = "AgentPositionReference";
+    private static final String AGENT_POSITION_EVENT_TIME = "AgentPositionEventTime";
+    private static final String AGENT_POSITION_PROCESSING_TIME = "AgentPositionProcessingTime";
 
     public static DefaultStreamProcessor createProcessor(DelaySimulationConfiguration delayConf,
                                                          StreamingApplicationConfiguration appConf,
                                                          StreamExecutionEnvironment env) {
-
         switch (appConf.getProcessorName()) {
-            case PRICE_MEAN_EVENT_TIME_PROCESSOR:
-                return new PriceMeanEventTime(delayConf, appConf, env);
-            case PRICE_MEAN_PROCESSING_TIME_PROCESSOR:
-                return new PriceMeanProcessingTime(delayConf, appConf, env);
             case PRICE_MEAN_REFERENCE:
                 return new PriceMeanReference(delayConf, appConf, env);
+            case PRICE_MEAN_EVENT_TIME:
+                return new PriceMeanEventTime(delayConf, appConf, env);
+            case PRICE_MEAN_PROCESSING_TIME:
+                return new PriceMeanProcessingTime(delayConf, appConf, env);
+            case AGENT_POSITION_REFERENCE:
+                return new AgentPositionReference(delayConf, appConf, env);
+            case AGENT_POSITION_EVENT_TIME:
+                return new AgentPositionEventTime(delayConf, appConf, env);
+            case AGENT_POSITION_PROCESSING_TIME:
+                return new AgentPositionProcessingTime(delayConf, appConf, env);
             default:
                 throw new StreamLayerException("Unable to find appropriate processor for the given name '"+appConf.getProcessorName()+"'");
         }
