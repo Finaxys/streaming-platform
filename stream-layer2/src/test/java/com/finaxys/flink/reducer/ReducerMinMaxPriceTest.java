6bbbd639585a88eb24a7757519d6affbac21e286
Khalil BEN AISSA <kbenaissa@finaxys.com>
2018-06-11 09:47:33 +0200

    Add PriceTest

---
+++ stream-layer2/src/test/java/com/finaxys/flink/reducer/ReducerMinMaxPriceTest.java
@@ -0,0 +1,29 @@
+package com.finaxys.flink.reducer;
+
+import com.finaxys.flink.function.reducer.ReduceMinMaxPrice;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReducerMinMaxPriceTest {
+
+    @Test
+    public void testSum() throws Exception {
+        // instantiate your function
+        ReduceMinMaxPrice reduceMinMaxPrice = new ReduceMinMaxPrice();
+        Tuple2<Integer, Integer> bestBid1 = new Tuple2<>(12,15);
+        Tuple2<Integer, Integer> bestBid2 = new Tuple2<>(1,10);
+        Tuple2<String, Tuple2<Integer, Integer>> t1 = new Tuple2<>("DD",bestBid1);
+        Tuple2<String, Tuple2<Integer, Integer>> t2 =  new Tuple2<>("DD",bestBid2);
+
+        Tuple2<String, Tuple2<Integer, Integer>> result = reduceMinMaxPrice.reduce(t1,t2);
+        Tuple2<Integer, Integer> resultBestBid = result.getField(1);
+
+
+        // call the methods that you have implemented
+        assertEquals("DD", result.getField(0));
+        assertEquals(1, (int)resultBestBid.getField(0));
+        assertEquals(15, (int)resultBestBid.getField(1));
+    }
+}
--- stream-layer2/src/test/java/com/finaxys/streamintegrator/model/PriceTest.java
+++ stream-layer2/src/test/java/com/finaxys/streamintegrator/model/PriceTest.java
@@ -11,7 +11,7 @@ public class PriceTest {
     @Test
     public void fromStringTest() throws Exception {
         String priceString = "Price;DD;8;9;A;Lucy-10381;Jerome-10231;3;10;1526725026492";
-        Price price = new Price("DD",8,9,"A","Lucy-10381","Jerome-10231",3,10);
+        Price price = new Price("ADD",8,9,"A","Lucy-10381","Jerome-10231",3,10);
         Price price2 = Price.fromString(priceString);
         assertEquals(price,price2);
     }
